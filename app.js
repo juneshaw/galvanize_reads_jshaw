c269d4d6b4b8145b35c5f6d216799ce888b8636c
June Shaw <june.shaw@me.com>
2016-02-08 17:41:28 -0700

    passport code in app.js

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ var routes = require('./routes/index');
 var authors = require('./routes/authors');
 var books = require('./routes/books');
 var login = require('./routes/login');
+var passport = require('passport');
+var LinkedInStrategy = require('passport-linkedin').Strategy
+
+
 
 var app = express();
 
@@ -23,6 +27,33 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LinkedInStrategy({
+    consumerKey: process.env.LINKEDIN_CLIENT_ID,
+    consumerSecret: process.env.LINKEDIN_CLIENT_SECRET,
+    callbackURL: process.env.HOST + "/auth/linkedin/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+    // To keep the example simple, the user's LinkedIn profile is returned to
+    // represent the logged-in user. In a typical application, you would want
+    // to associate the LinkedIn account with a user record in your database,
+    // and return that user instead (so perform a knex query here later.)
+    done(null, profile)
+  }
+));
+
+passport.serializeUser(function(user, done) {
+ // later this will be where you selectively send to the browser an identifier for your user,
+ // like their primary key from the database, or their ID from linkedin
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  //here is where you will go to the database and get the user each time from it's id, after you set up your db
+  done(null, user)
+});
 
 app.use('/', routes);
 app.use('/authors', authors);
