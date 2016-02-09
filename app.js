c53580bfaba2270a0ab9f46a27374b43475cca67
June Shaw <june.shaw@me.com>
2016-02-08 17:19:03 -0700

    more linkedin setup

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('cookie-session');
+require('dotenv').load();
+
 
 var routes = require('./routes/index');
 var authors = require('./routes/authors');
@@ -23,6 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ keys: [process.env.SESSION_KEY1, process.env.SESSION_KEY2] }))
 
 app.use('/', routes);
 app.use('/authors', authors);
