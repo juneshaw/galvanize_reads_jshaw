48fafa5998d59147af24db3689434940ddbd1bd0
June Shaw <june.shaw@me.com>
2016-02-09 21:22:48 -0700

    adding roles

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,7 @@ var router = express.Router();
 var passport = require('passport');
 var validate = require('../src/validation');
 var db = require('../src/db')
-
-
+var bcrypt = require('bcrypt')
 
 // Add a GET /auth/linkedin route that takes a middleware argument of passport.authenticate('linkedin').
 router.get('/linkedin',
@@ -14,12 +13,25 @@ router.get('/linkedin',
 // In /logout, set req.session to null and redirect to /.
 router.get('/logout', function(req, res, next) {
   req.session = null;
+  res.clearCookie("user");
   res.redirect('/');
 });
 
 // The route should be a GET request to /auth/linkedin/callback that takes a middleware argument of passport.authenticate('linkedin', { failureRedirect: '/' }). Inside the route you will simply redirect to /.
 router.get('/linkedin/callback', passport.authenticate('linkedin', {failureRedirect: '/'}), function(req, res, next) {
-  res.redirect('/');
+  res.cookie("user", req.user.username);
+  db.user(req.user.username).first().then(function(results) {
+    if (results) {
+      res.cookie("role", results.role);
+    } else {
+      db.insertUser({"user_name": req.user.username,
+                    "passwordHash": "",
+                    "role": "normal"}).then(function(results) {
+        res.cookie("role", "normal");
+      });
+    }
+    res.redirect('/');
+  })
 })
 
 router.get('/galvanize', function(req, res, next) {
@@ -30,22 +42,35 @@ router.get('/galvanize', function(req, res, next) {
 })
 
 router.post('/galvanize', function(req, res, next) {
-  var valPromise = validate.login(req.body);
-  valPromise.then(function(results) {
-    console.log('user results: ', results, 'req.body', req.body);
-    if (req.user) {
-    res.render('auth/index',
-                {'userName': req.body.userName,
-                'password': req.body.password,
-                'errors': results.errors});
-    } else {
-      res.render('auth/index',
-      {'userName': req.body.userName,
-      'password': req.body.password,
-      'errors': results.errors});
-    }
-
+  db.users().where('user_name', req.body.userName).first().then(function(result){
+    if (result) {
+       if (bcrypt.compareSync(req.body.password, found.password)) {
+         res.cookie("user", req.body.email, {secure:true});
+         res.redirect("/");
+       } else {
+         res.redirect("/auth/index");
+       }
+     } else {
+     res.redirect("/auth/index");
+   }
   })
+});
+  // var valPromise = validate.login(req.body);
+  // valPromise.then(function(results) {
+  //   console.log('user results: ', results, 'req.body', req.body);
+  //   if (req.user) {
+  //   res.render('auth/index',
+  //               {'userName': req.body.userName,
+  //               'password': req.body.password,
+  //               'errors': results.errors});
+  //   } else {
+  //     res.render('auth/index',
+  //     {'userName': req.body.userName,
+  //     'password': req.body.password,
+  //     'errors': results.errors});
+  //   }
+
+  // })
   //   console.log('no such user');
   //   if (user) {
   //     res.redirect('/index')
@@ -57,6 +82,6 @@ router.post('/galvanize', function(req, res, next) {
       // errors: results.errors})
     // }
   // })
-})
+// })
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,36 +1,33 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex')
+var db = require('../src/db')
 var bcrypt = require('bcrypt')
 
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
   var crypted = bcrypt.hashSync(req.body.password, 8);
-  Users().insert({email:req.body.email, password:crypted}).then(function(val){
-    res.redirect("/tickets");
+  db.users().insert({'email':req.body.email, 'passwordHash':crypted, 'role':'normal'}).then(function(val){
+    res.cookie("user", req.body.email, {secure:true});
+    res.redirect("/");
   });
 });
 
 router.post('/login', function(req, res, next) {
-    Users().where({email: req.body.email}).first().then(function(found){
-       if (found){
-         if bcrypt.compareSync(req.body.password, found.password) {
+  db.users().where('user_name' req.body.userName}).first().then(function(results){
+     if (results){
+       if bcrypt.compareSync(req.body.password, found.password) {
          res.cookie("user", req.body.email, {secure:true});
-         //or httpOnly:true
-         // or sign your cookies in app.js add a string to cookieparser:
-         // app.use(cookieparser("sometext"));
-         //AND res.cookie..., {sign:true}
-         // then change all req.cookie.user all
-         // occurrences to req.signed
-         res.redirect("/tickets");
+         res.cookie("role", results.role, {secure:true});
+         res.redirect("/");
        } else {
-         res.redirect("/no_auth");
+         res.redirect("/auth/index");
        }
      } else {
-       res.redirect("/no_auth");
-     }
-    })
+     res.redirect("/auth/index");
+   }
+  })
 });
 
 router.get('/', function(req, res, next) {
